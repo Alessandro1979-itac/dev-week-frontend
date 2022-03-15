30fc448fc26acce8e94458a94c365c92e80cc5be
Alessandro <sandromuniz27@hotmail.com>
2022-03-15 18:19:46 -0400

    fear: integração do front-end com back-end

--- src/app/ocorrencias/ocorrencias/ocorrencias.component.ts
+++ src/app/ocorrencias/ocorrencias/ocorrencias.component.ts
@@ -29,7 +29,9 @@ export class OcorrenciasComponent implements OnInit, OnDestroy {
   ) { }
 
   ngOnInit(): void {
-    this
+    this.listarRegioes();
+    this.listarOcorrencias();
+    this.listarFaixas();
   }
 
   ngOnDestroy(): void {
